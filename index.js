d796203e62a2272b83a6482d815868c1539a98e8
hefangshi <hefangshi@gmail.com>
2015-10-13 17:16:17 +0800

    [upd] sort file id before pack

--- index.js
+++ index.js
@@ -4,57 +4,61 @@
  */
 
 'use strict';
+var stable = require("stable");
 
-module.exports = function(ret, conf, settings, opt){
-    var pkgMap = {}, packed = {},
+module.exports = function (ret, conf, settings, opt) {
+    var pkgMap = {},
+        packed = {},
         ns = fis.config.get('namespace'),
         connector = fis.config.get('namespaceConnector', ':'),
         root = fis.project.getProjectPath();
     //construct package table
-    fis.util.map(conf, function(path, patterns, index){
-        if(typeof patterns === 'string' || patterns instanceof RegExp){
-            patterns = [ patterns ];
+    fis.util.map(conf, function (path, patterns, index) {
+        if (typeof patterns === 'string' || patterns instanceof RegExp) {
+            patterns = [patterns];
         }
-        if(fis.util.is(patterns, 'Array') && patterns.length){
+        if (fis.util.is(patterns, 'Array') && patterns.length) {
             var pid = (ns ? ns + connector : '') + 'p' + index,
                 subpath = path.replace(/^\//, ''),
                 pkg = fis.file(root, subpath);
-            if(typeof ret.src[pkg.subpath] !== 'undefined'){
+            if (typeof ret.src[pkg.subpath] !== 'undefined') {
                 fis.log.warning('there is a namesake file of package [' + path + ']');
             }
             pkgMap[pid] = {
-                id : pid,
-                file : pkg,
-                regs : patterns,
-                pkgs : new Array(patterns.length)
+                id: pid,
+                file: pkg,
+                regs: patterns,
+                pkgs: new Array(patterns.length)
             };
-        } else {
+        }
+        else {
             fis.log.warning('invalid pack config [' + path + ']');
         }
     });
-    
+
     //determine if subpath hit a pack config
-    var hit = function(subpath, regs){
-        for(var i = 0, len = regs.length; i < len; i++){
+    var hit = function (subpath, regs, file) {
+        for (var i = 0, len = regs.length; i < len; i++) {
             var reg = regs[i];
-            if(reg && fis.util.filter(subpath, reg)){
+            if (reg && fis.util.filter(subpath, reg)) {
                 return i;
             }
         }
         return false;
     };
-    
+
     //pack file
-    var pack = function(subpath, file){
-        if(packed[subpath] || file.isImage()) return;
-        fis.util.map(pkgMap, function(pid, pkg){
-            var index = hit(file.subpath, pkg.regs);
-            if(index !== false){
+    var pack = function (subpath, file, from) {
+        from = from || {};
+        if (packed[subpath] || file.isImage()) return;
+        fis.util.map(pkgMap, function (pid, pkg) {
+            var index = hit(file.subpath, pkg.regs, file);
+            if (index !== false) {
                 packed[subpath] = true;
-                file.requires.forEach(function(id){
+                file.requires.forEach(function (id) {
                     var dep = ret.ids[id];
-                    if(dep && dep.rExt === file.rExt){
-                        pack(dep.subpath, dep);
+                    if (dep && dep.rExt === file.rExt) {
+                        pack(dep.subpath, dep, file);
                     }
                 });
                 var stack = pkg.pkgs[index] || [];
@@ -65,28 +69,65 @@ module.exports = function(ret, conf, settings, opt){
             }
         });
     };
-    
+
+    function sortPkg(pkg) {
+        var sortedPkg = [];
+        var pkgFileMap = {};
+        var checked = {};
+        var ids = pkg.map(function (file) {
+            var id = file.getId();
+            pkgFileMap[id] = file;
+            return id;
+        });
+        stable.inplace(ids);
+        ids.forEach(checkRequire);
+
+        function checkRequire(id) {
+            if (checked[id]) {
+                return;
+            }
+            else {
+                checked[id] = true;
+            }
+            var file = ret.map.res[id];
+            file && file.deps && file.deps.forEach(function (depId) {
+                checkRequire(depId);
+            });
+            if (pkgFileMap[id]) {
+                sortedPkg.push(pkgFileMap[id]);
+            }
+        }
+        return sortedPkg;
+    }
+
+    var srcList = [];
+
     //walk
-    fis.util.map(ret.src, function(subpath, file){
+    fis.util.map(ret.src, function (subpath, file) {
         pack(subpath, file);
     });
-    
+
     //pack
-    fis.util.map(pkgMap, function(pid, pkg){
+    fis.util.map(pkgMap, function (pid, pkg) {
         //collect contents
-        var content = '', has = [], index = 0,
-            requires = [], requireMap = {};
-        pkg.pkgs.forEach(function(pkg){
-            pkg.forEach(function(file){
+        var content = '',
+            has = [],
+            index = 0,
+            requires = [],
+            requireMap = {};
+        pkg.pkgs.forEach(function (pkg) {
+            var sortedPkg = sortPkg(pkg);
+            sortedPkg.forEach(function (file) {
                 var id = file.getId();
-                if(ret.map.res[id]){
+                if (ret.map.res[id]) {
                     var c = file.getContent();
-                    if(c != ''){
-                        if(index++ > 0){
+                    if (c != '') {
+                        if (index++ > 0) {
                             content += '\n';
-                            if(file.isJsLike){
+                            if (file.isJsLike) {
                                 content += ';';
-                            } else if(file.isCssLike){
+                            }
+                            else if (file.isCssLike) {
                                 c = c.replace(/@charset\s+(?:'[^']*'|"[^"]*"|\S*);?/gi, '');
                             }
                         }
@@ -99,23 +140,23 @@ module.exports = function(ret, conf, settings, opt){
                 }
             });
         });
-        if(has.length){
+        if (has.length) {
             pkg.file.setContent(content);
             ret.pkg[pkg.file.subpath] = pkg.file;
             //collect dependencies
             var deps = [];
-            requires.forEach(function(id){
-                if(!requireMap[id]){
+            requires.forEach(function (id) {
+                if (!requireMap[id]) {
                     deps.push(id);
                     requireMap[id] = true;
                 }
             });
             var pkgInfo = ret.map.pkg[pid] = {
-                uri  : pkg.file.getUrl(opt.hash, opt.domain),
-                type : pkg.file.rExt.replace(/^\./, ''),
-                has  : has
+                uri: pkg.file.getUrl(opt.hash, opt.domain),
+                type: pkg.file.rExt.replace(/^\./, ''),
+                has: has
             };
-            if(deps.length){
+            if (deps.length) {
                 pkgInfo.deps = deps;
             }
         }
